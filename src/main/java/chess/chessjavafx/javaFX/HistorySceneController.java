e79454c574b4af462a3cb6e46f2caefa51858a4f
Adam Zieliński <adam.zie231@gmail.com>
2024-10-14 14:56:09 +0200

    swapped button for table

--- src/main/java/chess/chessjavafx/javaFX/HistorySceneController.java
+++ src/main/java/chess/chessjavafx/javaFX/HistorySceneController.java
@@ -1,10 +1,14 @@
 package chess.chessjavafx.javaFX;
 
 import chess.chessjavafx.game.GameMoves;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,17 +19,56 @@ public class HistorySceneController {
     private Scene scene;
     private VBox root;
     private Stage stage;
-    private List<Button> gameList;
+
+    private TableView<GameMoves> table;
+    private ObservableList<GameMoves> tableData;
 
     public HistorySceneController(Stage stage){
         this.root = new VBox(10);
         this.scene = new Scene(root);
-        this.gameList = new ArrayList<>();
         this.stage = stage;
+
+        this.table = new TableView<>();
+        this.tableData = FXCollections.observableArrayList();
+        table.setItems(tableData);
+        generateColumns();
 
         root.setMinWidth(1000);
         root.setMinHeight(1000);
         root.setAlignment(Pos.CENTER);
+
+        table.setItems(tableData);
+        root.getChildren().add(table);
+    }
+
+    private void generateColumns(){
+        TableColumn<GameMoves, String> dateCol = new TableColumn<>("Data");
+        dateCol.setCellValueFactory(new PropertyValueFactory<>("startTime"));
+        table.getColumns().add(dateCol);
+
+        TableColumn<GameMoves, String> lengthCol = new TableColumn<>("Długość");
+        lengthCol.setCellValueFactory(new PropertyValueFactory<>("duration"));
+        table.getColumns().add(lengthCol);
+
+        TableColumn<GameMoves, String> winnerCol = new TableColumn<>("Zwycięzca");
+        winnerCol.setCellValueFactory(new PropertyValueFactory<>("winner"));
+        table.getColumns().add(winnerCol);
+    }
+
+    private void generateRowClick(){
+        table.setRowFactory(tv -> {
+            TableRow<GameMoves> row = new TableRow<>();
+
+            row.setOnMouseClicked(event -> {
+                if(!row.isEmpty() && event.getClickCount() >= 1){
+                    GameMoves gameMoves = row.getItem();
+                    HistoryGameSceneController historyGameSceneController = new HistoryGameSceneController(gameMoves.getMoves());
+
+                    stage.setScene(historyGameSceneController.getScene());
+                }
+            });
+            return row;
+        });
     }
 
     public Scene getScene(){
@@ -33,18 +76,8 @@ public class HistorySceneController {
     }
 
     public void loadGames(List<GameMoves> gameMovesList){
-        for(GameMoves gameMoves : gameMovesList){
-            String formattedDuration = gameMoves.getDuration().toHoursPart() + ":"+gameMoves.getDuration().toMinutesPart();
-            Button button = new Button(gameMoves.getStartTime() +" "+ gameMoves.getWinner() +" "+ formattedDuration);
-
-            HistoryGameSceneController historyGameSceneController = new HistoryGameSceneController(gameMoves.getMoves());
-            button.setOnAction(event -> {
-                stage.setScene(historyGameSceneController.getScene());
-            });
-            gameList.add(button);
-
-            root.getChildren().add(button);
-        }
+        tableData.addAll(gameMovesList);
+        generateRowClick();
     }
 
 
