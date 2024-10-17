2c3cf2fbdf98959a22d2a3469a662db4c8201201
Adam Zieliński <adam.zie231@gmail.com>
2024-10-17 13:15:51 +0200

    add check if position is between 0 and 7

--- src/main/java/chess/chessjavafx/game/Position.java
+++ src/main/java/chess/chessjavafx/game/Position.java
@@ -5,18 +5,20 @@ public class Position {
     private int y;
 
     public Position(int x, int y){
+        checkIfValid(x, y);
         this.x = x;
         this.y = y;
     }
     public Position(int position){
+        checkIfValid(position);
         this.x = position % 8;
         this.y = position / 8;
     }
 
     public Position(String string){
+        checkIfValid(string);
         x = string.charAt(0)-'a';
         y = string.charAt(1) - '1';
-
     }
 
     public int getX() {
@@ -35,16 +37,40 @@ public class Position {
     }
 
     public void setPosition(int x, int y){
+        checkIfValid(x, y);
         this.x = x;
         this.y = y;
     }
     public void setPosition(Integer[] position){
+        checkIfValid(position);
         this.x = position[0];
         this.y = position[1];
     }
     public void setPosition(int position){
+        checkIfValid(position);
         this.x = position % 8;
         this.y = position / 8;
+    }
+
+    private void checkIfValid(int x, int y){
+        if(x < 0 || y > 7 || y < 0){
+            throw new IllegalArgumentException("Niepoprawna pozycja");
+        }
+    }
+    private void checkIfValid(int pos){
+        int x = pos % 8;
+        int y = pos / 8;
+        checkIfValid(x, y);
+    }
+    private void checkIfValid(Integer[] pos){
+        int x = pos[0];
+        int y = pos[1];
+        checkIfValid(x, y);
+    }
+    private void checkIfValid(String pos){
+        int x = pos.charAt(0)-'a';
+        int y = pos.charAt(1) - '1';
+        checkIfValid(x, y);
     }
 
     @Override
