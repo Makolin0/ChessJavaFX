da0c0c87d82d2296e7b4b9fd697811aba6061b1f
Adam Zieliński <adam.zie231@gmail.com>
2024-10-18 12:37:16 +0200

    added method lookForCheck

--- src/main/java/chess/chessjavafx/BoardController.java
+++ src/main/java/chess/chessjavafx/BoardController.java
@@ -9,22 +9,4 @@ import javafx.scene.shape.Rectangle;
 import java.util.*;
 
 public class BoardController {
-//    public boolean lookForCheck(Piece piece){
-//        // default for white
-//        System.out.println("Sprawdzam bicia");
-//
-//            System.out.println("Dla " + piece);
-//            if(piece.getTeam() == Piece.Team.WHITE){
-//                System.out.println("Bialy");
-//                for(Position position : piece.getBeatableList(pieceMap)){
-//                    Piece checkingPiece = pieceMap.get(position.getInt());
-//                    if("King".equals(checkingPiece.getClass().getSimpleName()) && checkingPiece.getTeam() != piece.getTeam()){
-//                        System.out.println("Szach");
-//                        infoPanel.getCheck().setText("Szach");
-//                    }
-//                }
-//            }
-//
-//        return false;
-//    }
 }
--- src/main/java/chess/chessjavafx/game/Checkerboard.java
+++ src/main/java/chess/chessjavafx/game/Checkerboard.java
@@ -2,11 +2,14 @@ package chess.chessjavafx.game;
 
 import chess.chessjavafx.pieces.*;
 import chess.chessjavafx.packages.Moveset;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import static chess.chessjavafx.pieces.Piece.Team.WHITE;
 
 public class Checkerboard {
     private Map<Integer, Piece> board;
@@ -28,31 +31,31 @@ public class Checkerboard {
         board.clear();
 
         for(int x = 0; x < 8; x++){
-            Piece pieceW = new Pawn(Piece.Team.WHITE);
+            Piece pieceW = new Pawn(WHITE);
             Piece pieceB = new Pawn(Piece.Team.BLACK);
             board.put(new Position(x, 1).getInt(), pieceW);
             board.put(new Position(x, 6).getInt(), pieceB);
         }
 
-        board.put(0, new Rook(Piece.Team.WHITE));
-        board.put(7, new Rook(Piece.Team.WHITE));
+        board.put(0, new Rook(WHITE));
+        board.put(7, new Rook(WHITE));
         board.put(56, new Rook(Piece.Team.BLACK));
         board.put(63, new Rook(Piece.Team.BLACK));
 
-        board.put(2, new Bishop(Piece.Team.WHITE));
-        board.put(5, new Bishop(Piece.Team.WHITE));
+        board.put(2, new Bishop(WHITE));
+        board.put(5, new Bishop(WHITE));
         board.put(58, new Bishop(Piece.Team.BLACK));
         board.put(61, new Bishop(Piece.Team.BLACK));
 
-        board.put(1, new Knight(Piece.Team.WHITE));
-        board.put(6, new Knight(Piece.Team.WHITE));
+        board.put(1, new Knight(WHITE));
+        board.put(6, new Knight(WHITE));
         board.put(57, new Knight(Piece.Team.BLACK));
         board.put(62, new Knight(Piece.Team.BLACK));
 
-        board.put(3, new Queen(Piece.Team.WHITE));
+        board.put(3, new Queen(WHITE));
         board.put(59, new Queen(Piece.Team.BLACK));
 
-        board.put(4, new King(Piece.Team.WHITE));
+        board.put(4, new King(WHITE));
         board.put(60, new King(Piece.Team.BLACK));
     }
 
@@ -86,5 +89,22 @@ public class Checkerboard {
         } else {
             throw new IllegalArgumentException("Pion nie może wykonać tego ruchu");
         }
+    }
+
+    public Piece.Team lookForCheck(){
+        for(Map.Entry<Integer, Piece> entry : board.entrySet()) {
+            Piece piece = entry.getValue();
+            Piece.Team team = piece.getTeam();
+            Position currentPos = new Position(entry.getKey());
+
+            piece.getBeatableList(board, currentPos);
+            for(Position pos : piece.getBeatableList(board, currentPos)){
+                if("King".equals(board.get(pos.getInt()).getClass().getName())){
+                    return piece.getTeam();
+                }
+            }
+        }
+
+        return null;
     }
 }
